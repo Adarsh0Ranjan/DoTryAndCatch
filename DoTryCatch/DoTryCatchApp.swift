193599110b9646f95c41c0522fea04e03fdca2a1
Adarsh Ranjan <adarsh.ranjan@roro.io>
2025-02-05 04:48:09 +0530

    Initial Commit

---
+++ DoTryCatch/DoTryCatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoTryCatchApp.swift
+//  DoTryCatch
+//
+//  Created by Adarsh Ranjan on 05/02/25.
+//
+
+import SwiftUI
+
+@main
+struct DoTryCatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
